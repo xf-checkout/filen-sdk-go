89ec420af697870fd9fdd1e1cf802b9f9087979a
Enduriel <endur1el@protonmail.com>
2025-04-11 09:40:06 +0100

    fix: timestamps not being correctly converted from seconds

--- filen/util/util.go
+++ filen/util/util.go
@@ -8,7 +8,7 @@ import (
 // TimestampToTime converts a timestamp to a time.Time.
 // It is not known whether the timestamp is in milliseconds or seconds.
 func TimestampToTime(timestamp int64) time.Time {
-	now := time.Now().Unix()
+	now := time.Now().UnixMilli()
 	if math.Abs(float64(now-timestamp)) < math.Abs(float64(now-timestamp*1000)) {
 		// ms timestamps
 		return time.UnixMilli(timestamp)
